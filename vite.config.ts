9c1114fed53211f9c58b01860e8fd6a79e8a8878
richmanstudio <133752573+richmanstudio@users.noreply.github.com>
2025-06-07 23:31:48 +1000

    little commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/tektonikaaa/',   // например "/tectonika-react/"
   plugins: [react()],
 })
